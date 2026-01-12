e7bc9ac033cd0cec29e6161484aedfe44735c6d3
bed-gan <440021905@gapps.yrdsb.ca>
2026-01-12 17:02:14 -0500

    added story world

---
+++ StoryWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * World for the story of the main character
+ * 
+ * @author Clifton Lin
+ * @version Jan, 2026
+ */
+public class StoryWorld extends World
+{
+    
+    /**
+     * Constructor for objects of class StoryWorld.
+     * 
+     */
+    public StoryWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
